74d1994b56c7d5e3596f837fcf46376736ca3bd4
parroty-exp <parroty-exp@example.com>
2022-01-22 14:12:51 +0000

    Add tentacat

--- mix.exs
+++ mix.exs
@@ -21,7 +21,8 @@ defmodule ExDemo.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~> 1.8.0"}
+      {:httpoison, "~> 1.8.0"},
+      {:tentacat, "~> 2.0.0"}
     ]
   end
 end
